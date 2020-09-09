5c356ca281cc02c0c81d4b9a25afe2e0a92f5676
Dowletgeldi99 <60849402+Dowletgeldi99@users.noreply.github.com>
2020-09-09 17:23:03 +0300

    The page of cook

---
+++ js/scripts.js
@@ -0,0 +1,226 @@
+/*
+   
+    Template Name : Rolling - Freelancer Portfolio Template
+    Author : UiPasta Team
+    Website : http://www.uipasta.com/
+    Support : http://www.uipasta.com/support/
+	
+	
+*/
+
+
+
+/*
+   
+   Table Of Content
+   
+   1. Preloader
+   2. Smooth Scroll
+   3. Scroll Naviagation Background Change with Sticky Navigation
+   4. Mobile Navigation Hide or Collapse on Click
+   5. Scroll To Top
+   6. Typed.js
+   7. Parallax Background
+   8. Portfolio Filtering
+   9. Magnific Popup
+  10. Testimonial Carousel/Slider
+  11. Statistics Counter
+  12. Google Map
+ 
+
+*/
+
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+        
+       /* Preloader */
+		
+        $(window).on('load', function() {
+          $('body').addClass('loaded');
+        });
+		
+		
+		
+       /* Плавная прокрутка */
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+		
+
+
+       
+       /* Изменение фона навигации прокрутки с помощью липкой навигации */
+		 
+        $(window).on('scroll', function () {
+            if ($(window).scrollTop() > 100) {
+                $('.header-top-area').addClass('navigation-background');
+            } else {
+                $('.header-top-area').removeClass('navigation-background');
+            }
+        });
+		
+		
+		
+		
+       /* Скрыть или свернуть по нажатию */
+		
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        
+		 });
+		 
+		
+		
+		
+        /* Пролистать наверх */
+		
+        $(window).scroll(function(){
+        if ($(this).scrollTop() >= 500) {
+            $('.scroll-to-top').fadeIn();
+         } else {
+            $('.scroll-to-top').fadeOut();
+         }
+	   });
+	
+	
+	    $('.scroll-to-top').click(function(){
+		  $('html, body').animate({scrollTop : 0},800);
+		  return false;
+	    });
+		
+		
+		
+		
+        /* Typed.js */
+		
+        $(window).load(function(){
+        $(".typing").typed({
+            strings: ["Я шеф повар.", "Я развиваю свою карьеру в ресторанном бизнесе.", "Я родился в Туркменистане живу в минске работаю поваром, у меня своя команда."],    /* You can change the home section typing text from
+	                                                                                            here and do not use "&" use "and" */
+            typeSpeed: 50
+          });
+         });
+        
+		 
+        /*задный фон */
+
+        $(window).stellar({
+            responsive: true,
+            horizontalScrolling: false,
+            hideDistantElements: false,
+            horizontalOffset: 0,
+            verticalOffset: 0,
+        });
+
+        
+		
+		
+        /* портфолио */
+
+        $('.portfolio-inner').mixItUp();
+
+
+       
+        /* всплывающее окно */
+
+        $('.portfolio-popup').magnificPopup({
+            type: 'image',
+			
+            gallery: { enabled: true },
+			zoom: { enabled: true,
+			        duration: 500
+					
+          },
+		  
+         image:{
+               markup: '<div class="mfp-figure portfolio-pop-up">'+
+               '<div class="mfp-close"></div>'+
+               '<div class="mfp-img"></div>'+
+               '<div class="mfp-bottom-bar portfolio_title">'+
+               '<div class="mfp-title"></div>'+
+               '<div class="mfp-counter"></div>'+
+               '</div>'+
+               '</div>',
+
+               titleSrc:function(item){
+                return item.el.attr('title');
+              }
+            }
+		  
+		  
+          });
+
+       
+	   
+		 
+        /* карусел, слайдер */
+
+        $(".testimonial-carousel-list").owlCarousel({
+            items: 1,
+            autoPlay: true,
+            stopOnHover: false,
+            navigation: true,
+            navigationText: ["<i class='fa fa-long-arrow-left fa-2x owl-navi'></i>", "<i class='fa fa-long-arrow-right fa-2x owl-navi'></i>"],
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [980, 1],
+            itemsTablet: [768, 1],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 1],
+            autoHeight: true,
+            pagination: false,
+            transitionStyle : "backSlide"
+        });
+		
+		
+		
+		
+        /* счетчик статистики */
+		
+        $('.statistics').appear(function() {
+           var counter = $(this).find('.statistics-count');
+           var toCount = counter.data('count');
+      
+           $(counter).countTo({
+           from: 0,
+           to: toCount,
+           speed: 5000,
+           refreshInterval: 50
+           })
+           });
+		   
+		  
+         
+         /* Google карта */
+		 
+         $('#my-address').gMap({
+            zoom: 5,
+            scrollwheel: true,
+            maptype: 'ROADMAP',
+            markers:[
+            {
+            address: "Минск",  /* вы можете изменить адрес здесь */
+            html: "<b>Address</b>: <br> Немига 5, Центральный район, Минск, Белоруссия",   /* You can change display address text from here */
+            popup: true
+            }
+            ]
+            });
+              
+		   
+            });
+
+   })(jQuery);
